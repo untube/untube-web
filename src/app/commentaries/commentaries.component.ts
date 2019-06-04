45c39ce3be25004182cbd75f7bf644abfe666f38
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2019-06-03 20:30:00 -0500

    Commit

--- src/app/commentaries/commentaries.component.ts
+++ src/app/commentaries/commentaries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Commentary } from '../shared/commentary';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-commentaries',
@@ -9,9 +10,14 @@ import { Commentary } from '../shared/commentary';
 export class CommentariesComponent implements OnInit {
 
   commentary: Commentary;
+  commentForm: any;
   constructor() { }
 
   ngOnInit() {
+    this.commentForm = new FormGroup({
+      'subject': new FormControl(this.commentary.subject, Validators.required),
+      'description': new FormControl(this.commentary.description, Validators.required)
+    })
   }
 
 }
